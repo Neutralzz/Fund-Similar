5eec89591f45bf0f0c453ff862259ce76e13f834
Neutralzz <lizhongli1234@163.com>
2018-03-29 09:17:34 +0800

    modify data api

--- src/web.py
+++ src/web.py
@@ -20,35 +20,44 @@ class DataHandler(tornado.web.RequestHandler):
     def get(self):
         global mongo_cli
         self.set_header('Content-Type', 'application/json; charset=UTF-8')
+        callback = self.get_argument('callback','')
         code = self.get_argument('code')
         date = self.get_argument('date',time.strftime("%Y-%m-%d", time.localtime()))
-        width = self.get_argument('len',20)
-        result = list(mongo_cli['fund-data'][code].find({'date':{'$lte':date}},{'date':1,'value':1,'_id':0}).limit(width))
+        width = int(self.get_argument('len','20'))
+        after = int(self.get_argument('after','0'))
+        result = list(mongo_cli['fund-data'][code].find({'date':{'$lte':date}},{'date':1,'value':1,'name':1,'_id':0}).limit(width))
         result.reverse()
+        if after != 0:
+            after_res = list(mongo_cli['fund-data'][code].find({'date':{'$gt':date}},{'date':1,'value':1,'name':1,'_id':0}).sort('date',pymongo.ASCENDING).limit(after))
+            while len(after_res) < after:
+                after_res.append({'date':'-','value':'-','name':'-'})
+            result = result + after_res
         #result = list(mongo_cli['fund-data'][code].find())[0]
-        self.write(json.dumps(result))
+        self.write(callback+'('+json.dumps(result)+')')
         self.finish()
 
 class ResultHandler(tornado.web.RequestHandler):
     def get(self):
         global mongo_cli
         self.set_header('Content-Type', 'application/json; charset=UTF-8')
+        callback = self.get_argument('callback','')
         code = self.get_argument('code')
         date = self.get_argument('date',time.strftime("%Y-%m-%d", time.localtime(time.time()-86400)))
-        width = self.get_argument('len',20)
+        width = int(self.get_argument('len','20'))
         result = list(mongo_cli['fund-similar'][code].find({'_id':date+','+str(width)}))
-        self.write(json.dumps(result))
+        self.write(callback+'('+json.dumps(result)+')')
         self.finish()
 
 class InfoHandler(tornado.web.RequestHandler):
     def get(self,input):
         global mongo_cli
         self.set_header('Content-Type', 'application/json; charset=UTF-8')
+        callback = self.get_argument('callback','')
         date = self.get_argument('date',time.strftime("%Y-%m-%d", time.localtime(time.time()-86400)))
-        width = self.get_argument('len',20)
+        width = int(self.get_argument('len','20'))
         if input == 'top':
             result = list(mongo_cli['fund-info']['similar-top'].find({'_id':date+','+str(width)}))
-            self.write(json.dumps(result))
+            self.write(callback+'('+json.dumps(result)+')')
         self.finish()
 
 def main():
